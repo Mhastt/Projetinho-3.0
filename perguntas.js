cf0ee87a03fa08b2e742829992bc5a8198750a3a
Mhastt <167993111+Mhastt@users.noreply.github.com>
2024-11-06 10:34:50 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Biologia',
+    'Do que os pandas se alimentam?',
+    'Pandas são carnívoros de nascença: Apesar de passarem o dia comendo bambu, os pandas são, na verdade, carnívoros! Eles possuem o sistema digestivo típico de carnívoros, mas se adaptaram a uma dieta quase totalmente herbívora.'
+)
+
+criaCartao(
+    'Biologia',
+    'O corpo humano brilha no escuro??',
+    'O corpo humano brilha no escuro (um pouco): Humanos emitem luz! Esse fenômeno, conhecido como "bioluminescência humana", é muito fraco para ser visto a olho nu. Esse brilho é resultado de reações químicas dentro das células.'
+)
+
+criaCartao(
+    'Biologia',
+    'Como os golfinhos se identificam entre si?',
+    'Golfinhos têm nomes próprios: Assim como nós, os golfinhos se identificam por "nomes". Cada golfinho cria um assobio único que os outros golfinhos usam para chamá-lo.'
+)
+
+criaCartao(
+    'Astronomia',
+    'A Terra já teve outras luas?',
+    'A Terra já teve mais de uma Lua: Existem teorias científicas que sugerem que a Terra pode ter tido mais de uma Lua em diferentes momentos de sua história. Uma teoria, chamada "hipótese da Lua dupla", sugere que outra pequena Lua orbitava a Terra, mas colidiu com a maior, o que pode ter criado o terreno irregular do lado oposto à Terra.'
+)
